d30b788f1653748c129850eae6e39251d0b78cb3
Yasin Derya Bilgin <127446168+ydbilgin@users.noreply.github.com>
2024-02-26 19:54:39 +0300

    .

--- src/components/Media/Media.jsx
+++ src/components/Media/Media.jsx
@@ -4,7 +4,7 @@ import "./Media.Style.css";
 const Media = ({ media }) => {
   return (
     <div className="question-media">
-      <img className="question-img" src={`src/assets/images/${media}`} alt="" />
+      <img className="question-img" src={media} alt="" />
     </div>
   );
 };
--- src/data.js
+++ src/data.js
@@ -1,4 +1,13 @@
-import cinSeddi from "../assets/images/cin-seddi.jpg";
+import cinSeddi from "./assets/images/cin-seddi.jpg";
+import pamuk from "./assets/images/pamuk.jpg";
+import tarkan from "./assets/images/tarkan.jpg";
+import fobi from "./assets/images/fobi.jpg";
+import balik from "./assets/images/balik.jpg";
+import bandira from "./assets/images/bandira.jpg";
+import billboard from "./assets/images/billboard.jpg";
+import parfum from "./assets/images/parfum.jpg";
+import trex from "./assets/images/trex.jpg";
+import mutluluk from "./assets/images/mutluluk.jpg";
 
 export const questions = [
   {
@@ -11,14 +20,14 @@ export const questions = [
     question: "İlk Pamuk şekeri bulan kişinin mesleği nedir?",
     options: ["Gıda Mühendisi", "Diş Doktoru", "Ev Hanımı", "Güzellik Uzmanı"],
     answer: "Diş Doktoru",
-    media: "pamuk.jpg",
+    media: pamuk,
   },
   {
     question:
       "Tarkan'ın 'Hüp' klibini izledikten sonra gaza gelip 'Tarkan keşke beni hüpletseydi' diye açıklamda bulunan kişi kimdir?",
     options: ["Gülben Ergen", "Hülya Avşar", "Harika Avcı", "Sevtap Parman"],
     answer: "Gülben Ergen",
-    media: "tarkan.jpg",
+    media: tarkan,
   },
   {
     question: "Pteronofobi nedir?",
@@ -29,46 +38,46 @@ export const questions = [
       "Kuş tüyüyle gıdıklanma korkusu",
     ],
     answer: "Kuş tüyüyle gıdıklanma korkusu",
-    media: "fobi.jpg",
+    media: fobi,
   },
   {
     question:
       "Ortalama ömürleri 5 yıl olan Japon balıklarının en uzun yaşayanı Tish, bütün istatistikleri alt üst ederek kaç yıl boyunca hayata tutunmayı başarmıştır?",
     options: ["43", "78", "23", "99"],
     answer: "43",
-    media: "balik.jpg",
+    media: balik,
   },
   {
     question:
       "90'lara damgasını vuran 'Bandıra Bandıra' şarkısının söz yazarı kimdir?",
     options: ["Sezen Aksu", "Sibel Can", "Mustafa Sandal", "Bülent Ersoy"],
     answer: "Mustafa Sandal",
-    media: "bandira.jpg",
+    media: bandira,
   },
   {
     question:
       "Hangi şarkıcımız yine kendisi gibi şarkıcı olan sevgilisinden ayrıldıktan sonra tam evinin karşısındaki apartmanın tamamını kendi posteriyle kaplatmıştır?",
     options: ["Hande Yener", "Hadise", "Gülşen", "Simge"],
     answer: "Hadise",
-    media: "billboard.jpg",
+    media: billboard,
   },
   {
     question: "Antik Roma'da kadınlar parfüm olarak ne kullanıyordu?",
     options: ["Gül Suyu", "Bal", "Gladyatör Teri", "Kan"],
     answer: "Gladyatör Teri",
-    media: "parfum.jpg",
+    media: parfum,
   },
   {
     question: "T-Rex'in yaşayan en yakın akrabası aşağıdakilerden hangisidir?",
     options: ["İnekler", "Tavuklar", "Timsahlar", "Köpekler"],
     answer: "Tavuklar",
-    media: "trex.jpg",
+    media: trex,
   },
   {
     question:
       "Her şeyin olduğu gibi mutluluğun da fobisi varmış. Bu fobiye ne ad verilir?",
     options: ["Çerofobi", "Euphobia", "Felicifobia", "Mutluluk Korkusu"],
     answer: "Çerofobi",
-    media: "fobi.jpg",
+    media: mutluluk,
   },
 ];
